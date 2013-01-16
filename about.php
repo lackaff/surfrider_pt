aebb3c1054a57b7ae5a9eaeacc714099f060167a
Madeline <mchapin@elon.edu>
2013-01-16 06:50:11 -0500

    updated mobile version of about page. added contact and problem pages

--- about.php
+++ about.php
@@ -11,7 +11,9 @@ Template Name: About
 <?php get_template_part( 'navbar'); ?> 
 
 
-  <!-- Carousel
+
+<div class="hidden-phone">
+    <!-- Carousel
     ================================================== -->
     <div class="headerpics">
         <div class="itempic">
@@ -91,7 +93,7 @@ Template Name: About
 		  <div class="span10">
 
 <p>NEED A PARAGRAPH ABOUT WHAT IT MEANS TO BE PART OF SURFRIDER </p>
-  <img src="img/corecircle.png" class="span2">
+  <img src="<?php echo get_template_directory_uri(); ?>/img/corecircle.png" class="span2">
 <!--  begining of accordian --> 
   <div class="accordion span7" id="accordion3">
   <!-- begining of accordian group --> 
@@ -103,7 +105,7 @@ Template Name: About
 				<div id="collapseOne" class="accordion-body collapse">
 					<div class="accordion-inner">
 						<div class="row">	
-					<img src="img/richard.png" class="span2" id="corepic">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/richard.png" class="span2" id="corepic">
 						<ul class="span4">
 							<li id="membername">Richard O'Sullivan</li>
 							<li id="membertitle">Responsible</li>
@@ -113,7 +115,7 @@ Template Name: About
 						</div>
 						
 						<div class="row">	
-					<img src="img/Osario.jpg" class="span2" id="corepic">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/Osario.jpg" class="span2" id="corepic">
 						<ul class="span4">
 							<li id="membername">Gonçalo Osario</li>
 							<li id="membertitle">Co-Responsible</li>
@@ -124,7 +126,7 @@ Template Name: About
 
 
 						<div class="row">	
-					<img src="img/placeholder.jpg" class="span2" id="corepic">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" class="span2" id="corepic">
 						<ul class="span4">
 							<li id="membername">Bruno Andrade </li>
 							<li id="membertitle">Treasurer</li>
@@ -134,7 +136,7 @@ Template Name: About
 						</div>
 						
 						<div class="row">	
-					<img src="img/hugo.jpg" class="span2" id="corepic">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/hugo.jpg" class="span2" id="corepic">
 						<ul class="span4">
 							<li id="membername">Hugo Pereira Rocha </li>
 							<li id="membertitle">Secretary</li>
@@ -145,7 +147,7 @@ Template Name: About
 						
 						
 						<div class="row">	
-					<img src="img/placeholder.jpg" class="span2" id="corepic">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" class="span2" id="corepic">
 						<ul class="span4">
 							<li id="membername">Bruno Lisboa</li>
 							<li id="membertitle">Volunteer Services Head </li>
@@ -155,7 +157,7 @@ Template Name: About
 						</div>						
 						
 						<div class="row">	
-					<img src="img/davide.jpg" class="span2" id="corepic">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/davide.jpg" class="span2" id="corepic">
 						<ul class="span4">
 							<li id="membername">Davide Troiani </li>
 							<li id="membertitle">Administrator</li>
@@ -238,7 +240,188 @@ Coastwatch Portugal researches and presents findings on wildlife, coast erosion 
     </div><!-- /.span10 offset1 -->
     </div><!-- /.row -->
 	</div><!-- /.container -->
+	
+</div><!-- ******************end of hidden phone *************************************** -->
+
+<div class="visible-phone">
+
+  <div class="container">
+	  <div class="row">
+		  <div class="span10 offset1">
+
+  
+<!--  begining of accordian --> 
+  <div class="accordion" id="accordion5">
+  <!-- begining of accordian group --> 
+	  <div class="accordion-group">
+		  <div class="accordion-heading">
+			  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion5" href="#collapseOne5">
+				  	About Us
+			</a>
+			</div>
+
+				<div id="collapseOne5" class="accordion-body collapse">
+					<div class="accordion-inner">
+<p>Surfrider Foundation Ericeira is a voluntary chapter of Surfrider Europe. </p>
+
+<p>We are an associated collective that shares the same goals of protecting and providing sustainable growth through education and positive activity in our local environment. Surfing is our passion. We want to protect our coastline’s beautiful resources that we so often take for granted. </p>
+
+<p>Surfrider Foundation Ericeira is a nonprofit organization based in Ericeira, Portugal and dedicated to the protection and enjoyment of the world’s oceans, waves and beaches for all people through conservation, activism, research and education. It is a voluntary chapter of Surfrider Foundation Europe. </p>
+
+<ul><li>C – Conservation through efforts like sand bagging, dune protection and water monitoring.</li>
+
+<li>A – Activism through lobbying, letters to the community, and peaceful protests. </li>
+
+<li>R – Research of new products as alternatives to pollutants, water testing results and effects on how construction on coastlines affects local ecosystems and foodchains.</li>
+
+<li>E – Education through school programs, promotional materials and new market products. </li>
+</ul>
+<p>Our mission is to create awareness and unity around the idea of maintaining Ericeira as one of the last paradises on earth. We want to create a bridge to new ideas and sustainable growth that benefits everyone in the Ericeira community, travelers and residents alike.</p>
+
+						<h2> Our Mission</h2>
+						<p>Our mission is to create awareness and unity around the idea of maintaining Ericeira as one of the last paradises on earth. We want to create a bridge to new ideas and sustainable growth that benefits everyone in the Ericeira community, travelers and residents alike. </p>		
+				</div>
+				</div>
+		</div>
+		<!-- end of accordian group --> 
+		
+  <!-- begining of accordian group --> 		
+<div class="accordion-group">
+<div class="accordion-heading">
+<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion5" href="#collapseTwo5">
+Our Staff
+</a>
+</div>
+<div id="collapseTwo5" class="accordion-body collapse">
+<div class="accordion-inner">
+   <img src="<?php echo get_template_directory_uri(); ?>/img/corecircle.png" class="span2">
+						<div class="row">	
+					<img src="<?php echo get_template_directory_uri(); ?>/img/richard.png" id="corepic">
+					<span id="membername">Richard O'Sullivan - Responsible</span>
+
+							<p id="memberp" class="span6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum dolor gravida neque condimentum euismod. Nulla velit odio, tempus et suscipit eget, blandit quis nibh. Maecenas in neque et magna auctor lacinia. Nulla facilisis congue porta. Vivamus fermentum, turpis eget laoreet mattis, odio lectus consectetur justo, id mattis nisl libero.</p>
+						</div>
+						
+						<div class="row">	
+					<img src="<?php echo get_template_directory_uri(); ?>/img/Osario.jpg" class="span1" id="corepic">
+						<span id="membername">Gonçalo Osario - Co-Responsible</span>
+							<p id="memberp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum dolor gravida neque condimentum euismod. Nulla velit odio, tempus et suscipit eget, blandit quis nibh. Maecenas in neque et magna auctor lacinia. Nulla facilisis congue porta. Vivamus fermentum, turpis eget laoreet mattis, odio lectus consectetur justo, id mattis nisl libero.</p>
+
+						</div>
 
 
+						<div class="row">	
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" class="span1" id="corepic">
+						<ul class="span4">
+						<span id="membername">Bruno Andrade - Treasurer</span>
+						
+							<p id="memberp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum dolor gravida neque condimentum euismod. Nulla velit odio, tempus et suscipit eget, blandit quis nibh. Maecenas in neque et magna auctor lacinia. Nulla facilisis congue porta. Vivamus fermentum, turpis eget laoreet mattis, odio lectus consectetur justo, id mattis nisl libero.</p>
+							</div>
+						
+						<div class="row">	
+					<img src="<?php echo get_template_directory_uri(); ?>/img/hugo.jpg" class="span2" id="corepic">
+						<ul class="span4">
+							<span id="membername">Hugo Pereira Rocha - Secretary</span>
+							
+							<p id="memberp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum dolor gravida neque condimentum euismod. Nulla velit odio, tempus et suscipit eget, blandit quis nibh. Maecenas in neque et magna auctor lacinia. Nulla facilisis congue porta. Vivamus fermentum, turpis eget laoreet mattis, odio lectus consectetur justo, id mattis nisl libero.</p>
+	
+						</div>
+						
+						
+						<div class="row">	
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" class="span2" id="corepic">
+						<ul class="span4">
+							<span id="membername">Bruno Lisboa - Volunteer Services Head</span>
+
+							<p id="memberp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum dolor gravida neque condimentum euismod. Nulla velit odio, tempus et suscipit eget, blandit quis nibh. Maecenas in neque et magna auctor lacinia. Nulla facilisis congue porta. Vivamus fermentum, turpis eget laoreet mattis, odio lectus consectetur justo, id mattis nisl libero.</p>
+	
+						</div>						
+						
+						<div class="row">	
+					<img src="<?php echo get_template_directory_uri(); ?>/img/davide.jpg" class="span2" id="corepic">
+						<ul class="span4">
+							<span id="membername">Davide Troiani  - Administrator</span>
+
+							<p id="memberp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum dolor gravida neque condimentum euismod. Nulla velit odio, tempus et suscipit eget, blandit quis nibh. Maecenas in neque et magna auctor lacinia. Nulla facilisis congue porta. Vivamus fermentum, turpis eget laoreet mattis, odio lectus consectetur justo, id mattis nisl libero.</p>
+	
+						</ul>
+						</div>
+
+		
+
+					<ul>
+						<li>Richard O'Sullivan</li>
+						<li>other person</li>
+						<li>another person</li>
+					</ul>
+				 </div>
+</div>
+</div>
+
+  <!-- end of accordian group --> 
+
+  
+  
+   <!-- begining of accordian group --> 		
+<div class="accordion-group">
+<div class="accordion-heading">
+<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion5" href="#collapseThree5">
+Partners
+</a>
+</div>
+<div id="collapseThree5" class="accordion-body collapse">
+<div class="accordion-inner">
+<p>Surfrider works with a number of different groups in the Ericeira community to help achieve its mission. Without their help, protecting the local coastlines would certainly be a more difficult task to tackle. </p>
+
+<p>Ericeira City Hall grants Surfrider Ericerira the permission we need to be active in the community. It also aids in funding and collects taxes.
+Coastwatch Portugal researches and presents findings on wildlife, coast erosion and statistics for the area. 
+</p>
+
+</div>
+</div>
+</div>
+
+  <!-- end of accordian group --> 
+
+  
+  
+   <!-- begining of accordian group --> 		
+<div class="accordion-group">
+<div class="accordion-heading">
+<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion5" href="#collapseFour5">
+Sustainable Surfing
+</a>
+</div>
+<div id="collapseFour5" class="accordion-body collapse">
+<div class="accordion-inner">
+<p>Though the action of riding a wave is environmentally friendly, there are ways the lifestyle and industry associated with it can be improved to have less negative impact on the environment. Here are some of Surfrider’s quick tips to surfing sustainably. </p>
+
+						<ul>
+							<li>Purchase your equipment from companies that localize production. Despite how well a product may be made by a certain brand, the petroleum involved in transport can pose a significant threat to the health of the environment. </li>
+							<li>Choose to purchase from brands who place emphasis on using materials that are good for the environment. Manufacturers can reduce the toxicity of surfboard and wetsuit production by using alternative materials instead. By opting to support companies that do with your purchase, you help spur a small amount of change in the industry. </li>
+							<li>Shop locally from retailers who deal with waste and energy use responsibly. It’s great if a surfing business caries great products, but if it’s reckless in how it disposes of the waste that comes with those materials, you’re just contributing to more toxic waste from the surfing industry. </li>
+							<li>Make less surf trips and drive less. If you have the option to surf a full day once a week versus one hour five days a week, you can significantly reduce the amount of fuel you use in travel. </li>
+							<li>Seek out more ways to create less waste on your own. Be as wary of the impact you have on the environment as you can. </li>
+						</ul>
+						<p>By following these tips, you can help make sure the waters you choose to surf in are clean and healthy for you, your friends and the rest of the surfing community. </p>
+</div>
+</div>
+</div>
+
+  <!-- end of accordian group --> 
+
+  </div><!-- end of accordian --> 
+  </div><!-- /.span10 offset1 -->
+  </div><!-- /.row -->
+  </div> <!-- /.container -->
+
+
+
+
+
+
+</div>	<!-- end of visible phone -->
+	
+	
 
 <?php get_footer(); ?>
---
+++ contact.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+Template Name: Contact
+*/
+?>
+
+
+<?php get_header(); ?>
+
+<?php get_template_part( 'navbar'); ?> 
+
+
+    <!-- Carousel
+    ================================================== -->
+    <div class="headerpics">
+        <div class="fullpic">
+	          	<img   src="<?php echo get_template_directory_uri(); ?>/img/slide-01.jpg" alt="">
+	    </div>
+	    
+    </div><!-- /.head pic -->
+
+
+	
+	  <div class="container">
+	  <div class="row">
+		  <div class="span10 offset1">
+<h2>Contact Us</h2>
+		<div class="span4">
+		<div class="row">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="contacticon span1" />
+			<span class="span3">Address</span>
+		</div><!-- row --> 
+		
+		<div class="row">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="contacticon span1" />
+			<span class="span3">508-930-4559</span>
+		</div><!-- row --> 
+		
+		<div class="row">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="contacticon span1" />
+			<span class="span3">mchapin@elon.edu</span>
+		</div><!-- row --> 
+		
+		
+		<div class="row">
+		<h4>Connect with us on Social Media</h4>
+		<div class="row">
+					<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
+					<a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
+					<img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb">
+				</div>
+		</div><!-- row --> 
+		  
+		 </div><!-- close span 4 left -->  
+		  
+		<div class="span5">
+		<h4>Find your local representative</h4>
+				  
+		</div><!-- close span 6 rightt --> 
+		  
+		  
+		  
+		  
+		  </div></div></div>
+
+
+<?php get_footer(); ?>
