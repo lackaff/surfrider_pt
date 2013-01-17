20ea4ba6c7b5792f297434f1c711e190465acbbc
Derek Lackaff <lackaff@lackaff.net>
2013-01-17 18:49:07 -0500

    remove hardlinks in navbar

--- navbar.php
+++ navbar.php
@@ -14,12 +14,12 @@ Template Name: navbar
           <div class="navbar-inner">
             <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
             <a class="btn btn-navbar" id="menubtn" data-toggle="collapse" data-target=".nav-collapse">Menu</a>
-            <a class="brand" href="#">
+            <a class="brand" href="<?php bloginfo('url'); ?>">
 	            
 	            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt"surfrider foundation Europe logo" id="logoimg"/>
 	            
             </a>
-            <a href="http://madelinechapin.com/portugaltest/"><h1>Ericeira</h1></a>
+            <a href="<?php bloginfo('url'); ?>/"><h1>Ericeira</h1></a>
             <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
            
            
@@ -30,24 +30,24 @@ Template Name: navbar
               
               
               
-               <li><a href="http://madelinechapin.com/portugaltest/about/">About Us</a></li>
+               <li><a href="<?php bloginfo('url'); ?>/about/">About Us</a></li>
 		
 		<li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Learn<span class="caret"></span></a>
 			<ul class="dropdown-menu">
-				<li><a href="http://madelinechapin.com/portugaltest/problem/">The Problem</a></li>
-				<li><a href="http://madelinechapin.com/portugaltest/resources/">Resources</a></li>
-				<li><a href="http://madelinechapin.com/portugaltest/psa/">PSA's</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/problem/">The Problem</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/resources/">Resources</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/psa/">PSA's</a></li>
 			</ul>
 		</li>
 		
 			
 		<li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Take Action<span class="caret"></span></a>
 			<ul class="dropdown-menu">
-				<li><a href="http://madelinechapin.com/portugaltest/why-give/">Why Give?</a></li>
-				<li><a href="http://madelinechapin.com/portugaltest/volunteer/">Volunteer</a></li>
-				<li><a href="#">Donate</a></li>
-				<li><a href="http://madelinechapin.com/portugaltest/membership/">Become a Member</a></li>
-				<li><a href="http://madelinechapin.com/portugaltest/calendar/">Events Calendar</a> </li>
+				<li><a href="<?php bloginfo('url'); ?>/why-give/">Why Give?</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/volunteer/">Volunteer</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/donate/">">Donate</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/membership/">Become a Member</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/calendar/">Events Calendar</a> </li>
 			</ul>
 		</li>	
 		
@@ -59,9 +59,9 @@ Template Name: navbar
 		</li>
 		
 
-		<li> <a href="http://madelinechapin.com/portugaltest/contact/">Contact Us</a></li>	
+		<li> <a href="<?php bloginfo('url'); ?>/contact/">Contact Us</a></li>	
 
-		<li> <a href="http://madelinechapin.com/portugaltest/kid-zone/" ><img src="<?php echo get_template_directory_uri(); ?>/img/kidzone.png" class="visible-desktop" id="kidzone"><span class="hidden-desktop" id="kidzonemobile">Kid Zone </span></a></li>	
+		<li> <a href="<?php bloginfo('url'); ?>/kid-zone/" ><img src="<?php echo get_template_directory_uri(); ?>/img/kidzone.png" class="visible-desktop" id="kidzone"><span class="hidden-desktop" id="kidzonemobile">Kid Zone </span></a></li>	
               </ul>
             </div><!--/.nav-collapse -->
           </div><!-- /.navbar-inner -->
