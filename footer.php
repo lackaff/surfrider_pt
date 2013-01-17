9bc8740583027be559b0380390a9d3606eebf102
Madeline <mchapin@elon.edu>
2013-01-17 14:36:59 -0500

    updated footer

--- footer.php
+++ footer.php
@@ -1,29 +1,116 @@
-			<footer role="contentinfo">
-			
-				<div id="inner-footer" class="clearfix">
-		          <hr />
-		          <div id="widget-footer" class="clearfix row-fluid">
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
-		            <?php endif; ?>
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
-		            <?php endif; ?>
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
-		            <?php endif; ?>
-		          </div>
-					
-					<nav class="clearfix">
-						<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
-					</nav>
-					
-					<p class="pull-right"><a href="http://320press.com" id="credit320" title="By the dudes of 320press">320press</a></p>
-			
-					<p class="attribution">&copy; <?php bloginfo('name'); ?></p>
+	<footer class="foot">
+  <div class="container visible-desktop">
+      <div class="row">
+      
+	      <div class="span3">
+		      
+		     <ul>
+			<li>About Us</li>
+				<ul>
+					<li>Who Are We</li>
+					<li>Our Mission</li>
+					<li>Our Partners</li>
+					<li>Our Staff</li>
+					<li>Surf Related</li>
+					<li>FAQ's</li>
+				</ul>
+			<li>Learn</li>
+				<ul>
+					<li>The Problem</li>
+					<li>Resources</li>
+					<li>PSA's</li>
+					<li>Games</li>
+				</ul>
+									</ul>
+	
+		      
+	      </div> <!-- end span 3 -->
+	       
+	      <div class="span3 visible-desktop">
+					      
+					<ul>	
+							<li>Take Action</li>
+				<ul>
+					<li>Why Give?</li>
+					<li>Volunteer</li>
+					<li>Ways To Volunteer</li>
+					<li>Donate</li>
+					<li>Ways to Donate</li>
+					<li>Become a Member</li>
+					<li>Events Calendar</li> 
+				</ul>
+
+							<li>Blog</li>
+							<ul><li>Photo's</li><li>Video</li></ul>
 				
-				</div> <!-- end #inner-footer -->
-				
-			</footer> <!-- end footer -->
-		
-		</div> <!-- end #container -->
+						<li>Contact</li>
+						<li id="kidszone">Kids' Zone</li>
+					</ul>
+
+	      </div> <!-- end span 3 -->
+
+
+	      <div class="span4 offset2">
+			      	<div class="row">
+					<h1 class="span4">Ericeira</h1>
+				</div>
+				<div class="row">
+					<h4 class="span4">Surfrider Foundation Europe</h4>
+				</div>
+				<div class="row">
+					<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="img/facebook.png" class="socialthumb" ></a>
+					<a href="https://twitter.com/surfridericeira" target="_blank"><img src="img/twitter.png" class="socialthumb"></a>
+					<a href="#" target="_blank"><img src="img/instagram.png" class="socialthumb"></a>
+					<a href="#" target="_blank"><img src="img/youtube.png" class="socialthumb"></a>
+				</div>
+				<div class="row">
+					<h3 class="span4">Contact Information</h3>
+				</div>
+				<div class="row">
+					<h3 class="span4">Sponsors</h3>
+				</div>
+
+	      	
+	      </div><!-- end span 4 -->
+      </div><!-- /.row -->
+  </div><!-- /.container -->
+  
+  
+  
+  
+    <div class="container hidden-desktop">
+
+	      <div class="span6 offset3 pull-center">
+			      	<div class="row">
+					<h1 class="span6">Ericeira</h1>
+				</div>
+				<div class="row">
+					<h4 class="span6">Surfrider Foundation Europe</h4>
+				</div>
+				<div class="row">
+					<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="img/facebook.png" class="socialthumb" ></a>
+					<a href="https://twitter.com/surfridericeira" target="_blank"><img src="img/twitter.png" class="socialthumb"></a>
+					<a href="#" target="_blank"><img src="img/instagram.png" class="socialthumb"></a>
+					<a href="#" target="_blank"><img src="img/youtube.png" class="socialthumb"></a>
+				</div>
+				<div class="row">
+					<h3 class="span6">Contact Information</h3>
+				</div>
+				<div class="row">
+					<h3 class="span6">Sponsors</h3>
+				</div>
+
+	      	
+	      </div><!-- end span 4 -->
+      </div><!-- /.row -->
+  </div><!-- /.container -->
+  
+  
+  
+  
+  
+  </footer>
+
 				
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
