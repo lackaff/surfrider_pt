75d579a93bb77815ff07289a263c4bdb2675135a
Derek Lackaff <lackaff@lackaff.net>
2013-01-18 14:41:19 -0500

    update admin footer

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ require_once('library/shortcodes.php');
 // Custom Backend Footer
 add_filter('admin_footer_text', 'bones_custom_admin_footer');
 function bones_custom_admin_footer() {
-	echo '<span id="footer-thankyou">Developed by <a href="http://320press.com" target="_blank">320press</a></span>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.';
+	echo '<span id="footer-thankyou">Developed by <a href="http://madelinechapin.com">Madeline Chapin</a> and <a href="http://www.elon.edu/imedia">Elon iMedia</a> Team Portugal.</span>. Based on Wordpress-Bootstrap by <a href="http://320press.com" target="_blank">320press</a>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.';
 }
 
 // adding it to the admin area
