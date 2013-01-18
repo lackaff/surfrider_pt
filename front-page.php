6dbf374c534e62e5cf733db83c38de58d22362fe
Madeline <mchapin@elon.edu>
2013-01-18 17:36:37 -0500

    added new content to main slider and made css changes to the slider

--- front-page.php
+++ front-page.php
@@ -13,32 +13,42 @@ Template Name: home
     <div id="myCarousel" class="carousel slide">
       <div class="carousel-inner">
         <div class="item">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/slide2test.JPG" alt="">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/sliderLearn.jpg" alt="">
           <div class="container">
             <div class="carousel-caption" id="headline1">
-              <h1 >Example headline.</h1>
-              <p class="lead hidden-phone">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <a class="btn btn-large btn-primary hidden-phone" href="#">Slide 1</a>
+              <h1 >Give Back!</h1>
+             <p class="lead hidden-phone">Give back what you take from the ocean.</p>
+              <a class="btn btn-large btn-primary hidden-phone" href="<?php bloginfo('url'); ?>/problem/">Learn more</a>
             </div>
           </div>
         </div>
         <div class="item">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/slide1test.JPG" alt="">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/sliderBeachClean.jpg" alt="">
           <div class="container">
             <div class="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p class="lead hidden-phone">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <a class="btn btn-large btn-primary hidden-phone" href="#">Slide 2</a>
+              <h1>Community Involvement</h1>
+              <p class="lead hidden-phone">Secondary school students sweep the shore for trash.</p>
+              <a class="btn btn-large btn-primary hidden-phone" href="#">Read the blog</a>
             </div>
           </div>
         </div>
         <div class="item active">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/slide3test.JPG	" alt="">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/sliderConnect.jpg" alt="">
           <div class="container">
             <div class="carousel-caption">
-              <h1>One more for good measure.</h1>
-              <p class="lead hidden-phone">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <a class="btn btn-large btn-primary hidden-phone" href="#">Slide 3</a>
+              <h1>Connect</h1>
+              <p class="lead hidden-phone">Follow us on social media for updates about our coast.</p>
+              <a class="btn btn-large btn-primary hidden-phone" href="<?php bloginfo('url'); ?>/contact/">Connect</a>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/sliderVolunteer.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Volunteer.</h1>
+              <p class="lead hidden-phone">Participate in our next beach cleanup.</p>
+              <a class="btn btn-large btn-primary hidden-phone" href="<?php bloginfo('url'); ?>/contact/">Volunteer.</a>
             </div>
           </div>
         </div>
@@ -58,16 +68,13 @@ Template Name: home
         <div class="span4">
           <img class="img-circle" data-src="holder.js/140x140">
           <h2>Calendar</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis 
-euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi 
-leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo
- cursus magna, vel scelerisque nisl consectetur et.</p>
-          <p><a class="btn" href="carousel.html#">View details »</a></p>
+				<?php echo do_shortcode('[events_list limit="5"]#_EVENTLINK - #_EVENTDATES<br /><br />[/events_list]'); ?>
+
         </div><!-- /.span4 -->
-        
-        <div class="span4" id="blogindex">
+         
+        <div class="span4">
           <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Blog</h2>
+          <h2 class="fpcenter">Blog</h2>
            <?php $args = array( 'numberposts' => 6, 'post_status'=>"publish",'post_type'=>"post",'orderby'=>"post_date"); $postslist = get_posts( $args ); foreach ($postslist as $post) : setup_postdata($post); ?> <div class="events">  <p><a id="blogindex" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></p> </div><?php endforeach; ?> 
            
             
@@ -75,7 +82,7 @@ leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo
         
         <div class="span4">
           <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Take Action</h2>
+          <h2 class="fpcenter">Take Action</h2>
           <div class="row">
           <img class="span2" src="<?php echo get_template_directory_uri(); ?>/img/ta1.jpg" />
           <p class="span2">Donec sed odio dui. Cras justo odio, dapibus ac facilisis 
@@ -102,7 +109,7 @@ in, egestas eget quam.</p>
       
         <div class="span6">
           <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Calendar</h2>
+          <h2 class="fpcenter">Calendar</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis 
 euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi 
 leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo
