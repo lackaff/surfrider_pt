4576db155e57330e035febd1d80ef5fb767a29ff
Derek Lackaff <lackaff@lackaff.net>
2013-01-17 20:05:25 -0500

    remove hardlinks in footer

--- footer.php
+++ footer.php
@@ -1,115 +1,115 @@
 	<footer class="foot">
-  <div class="container visible-desktop">
-      <div class="row">
-      
-	      <div class="span3">
-		      
-		     <ul>
-			<li><a href="http://madelinechapin.com/portugaltest/about/">About Us</a></li>
-			
-			<li>Learn</li>
-				<ul>
-					<li><a href="http://madelinechapin.com/portugaltest/problem/">The Problem</a></li>
-					<li><a href="http://madelinechapin.com/portugaltest/resources/">Resources</a></li>
-					<li><a href="http://madelinechapin.com/portugaltest/psa/">PSAs</a></li>
-				</ul>
-		</ul>
-	
-		      
-	      </div> <!-- end span 3 -->
-	       
-	      <div class="span3 visible-desktop">
-					      
-					<ul>	
-							<li>Take Action</li>
-				<ul>
-					<li><a href="http://madelinechapin.com/portugaltest/why-give/">Why Give?</a></li>
-					<li><a href="http://madelinechapin.com/portugaltest/volunteer/">Volunteer</a></li>
-					<li><a href="#">Donate</a></li>
-					<li><a href="http://madelinechapin.com/portugaltest/membership/">Become a Member</a></li>
-					<li><a href="http://madelinechapin.com/portugaltest/calendar/">Events Calendar</a> </li>
-				</ul>
-
-							<li>Blog</li>
-							<ul><li>Photo's</li>
-                            <li>Video</li></ul>
+		<div class="container visible-desktop">
+			<div class="row">
 				
-						<li> <a href="http://madelinechapin.com/portugaltest/contact/">Contact Us</a></li>
-						<li> <a href="http://madelinechapin.com/portugaltest/kid-zone/" >Kids' Zone</a></li>
+				<div class="span3">
+					
+					<ul>
+						<li><a href="<?php bloginfo('url'); ?>/about/">About Us</a></li>
+						
+						<li>Learn</li>
+						<ul>
+							<li><a href="<?php bloginfo('url'); ?>/problem/">The Problem</a></li>
+							<li><a href="<?php bloginfo('url'); ?>/resources/">Resources</a></li>
+							<li><a href="<?php bloginfo('url'); ?>/psa/">PSAs</a></li>
+						</ul>
 					</ul>
-
-	      </div> <!-- end span 3 -->
-
-
-	      <div class="span4 offset2">
-			      	<div class="row">
-					<h1 class="span4">Ericeira</h1>
-				</div>
-				<div class="row">
-					<h4 class="span4">Surfrider Foundation Europe</h4>
-				</div>
-				<div class="row">
-					<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
-					<a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
-					<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb"></a>
-					<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb"></a>
-				</div>
-				<div class="row">
-					<h3 class="span4">Contact Information</h3>
-				</div>
-				<div class="row">
-					<h3 class="span4">Sponsors</h3>
-				</div>
-
-	      	
-	      </div><!-- end span 4 -->
-      </div><!-- /.row -->
-  </div><!-- /.container -->
-  
-  
-  
-  
-    <div class="container hidden-desktop">
-
-	      <div class="span6 offset3 pull-center">
-			      	<div class="row">
-					<h1 class="span6">Ericeira</h1>
-				</div>
-				<div class="row">
-					<h4 class="span6">Surfrider Foundation Europe</h4>
-				</div>
-				<div class="row">
-					<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
-					<a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
-					<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb"></a>
-					<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb"></a>
-				</div>
-				<div class="row">
-					<h3 class="span6">Contact Information</h3>
-				</div>
-				<div class="row">
-					<h3 class="span6">Sponsors</h3>
-				</div>
-
-	      	
-	      </div><!-- end span 4 -->
-      </div><!-- /.row -->
-  </div><!-- /.container -->
-  
-  
-  
-  
-  
-  </footer>
-
+					
+					
+				</div> <!-- end span 3 -->
 				
+				<div class="span3 visible-desktop">
+					
+					<ul>	
+						<li>Take Action</li>
+						<ul>
+							<li><a href="<?php bloginfo('url'); ?>/why-give/">Why Give?</a></li>
+							<li><a href="<?php bloginfo('url'); ?>/volunteer/">Volunteer</a></li>
+							<li><a href="#">Donate</a></li>
+							<li><a href="<?php bloginfo('url'); ?>/membership/">Become a Member</a></li>
+							<li><a href="<?php bloginfo('url'); ?>/calendar/">Events Calendar</a> </li>
+						</ul>
+
+						<li>Blog</li>
+						<ul><li>Photo's</li>
+							<li>Video</li></ul>
+							
+							<li> <a href="<?php bloginfo('url'); ?>/contact/">Contact Us</a></li>
+							<li> <a href="<?php bloginfo('url'); ?>/kid-zone/" >Kids' Zone</a></li>
+						</ul>
+
+					</div> <!-- end span 3 -->
+
+
+					<div class="span4 offset2">
+						<div class="row">
+							<h1 class="span4">Ericeira</h1>
+						</div>
+						<div class="row">
+							<h4 class="span4">Surfrider Foundation Europe</h4>
+						</div>
+						<div class="row">
+							<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
+							<a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
+							<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb"></a>
+							<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb"></a>
+						</div>
+						<div class="row">
+							<h3 class="span4">Contact Information</h3>
+						</div>
+						<div class="row">
+							<h3 class="span4">Sponsors</h3>
+						</div>
+
+						
+					</div><!-- end span 4 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+			
+			
+			
+			
+			<div class="container hidden-desktop">
+
+				<div class="span6 offset3 pull-center">
+					<div class="row">
+						<h1 class="span6">Ericeira</h1>
+					</div>
+					<div class="row">
+						<h4 class="span6">Surfrider Foundation Europe</h4>
+					</div>
+					<div class="row">
+						<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
+						<a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
+						<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb"></a>
+						<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb"></a>
+					</div>
+					<div class="row">
+						<h3 class="span6">Contact Information</h3>
+					</div>
+					<div class="row">
+						<h3 class="span6">Sponsors</h3>
+					</div>
+
+					
+				</div><!-- end span 6 -->
+
+			</div><!-- /.container -->
+			
+			
+			
+			
+			
+		</footer>
+
+		
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
   			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-		<![endif]-->
-		
-		<?php wp_footer(); // js scripts are inserted using this function ?>
+  			<![endif]-->
+  			
+  			<?php wp_footer(); // js scripts are inserted using this function ?>
 
-	</body>
+  		</body>
 
-</html>
+  		</html>
