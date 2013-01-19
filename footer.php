fc7c3f9e288108990e2cbc478a2d6f02349a80b2
Madeline <mchapin@elon.edu>
2013-01-18 19:56:08 -0500

    changed footer

--- footer.php
+++ footer.php
@@ -10,75 +10,80 @@
 }(document, 'script', 'facebook-jssdk'));</script>
         
         <div class="container visible-desktop">
+        <div >
 			<div class="row">
-				
-				<div class="span3">
-					
-					<ul>
-						<li><a href="<?php bloginfo('url'); ?>/about/">About Us</a></li>
-						
-						<li>Learn</li>
-						<ul>
-							<li><a href="<?php bloginfo('url'); ?>/problem/">The Problem</a></li>
-							<li><a href="<?php bloginfo('url'); ?>/resources/">Resources</a></li>
-							<li><a href="<?php bloginfo('url'); ?>/psa/">PSAs</a></li>
-						</ul>
-                        <li>Blog</li>
-                        <ul>
-                        	<li><a href="<?php bloginfo('url'); ?>/surfrider-blog/">Blog</a></li>
-							<li><a href="<?php bloginfo('url'); ?>/photos/">Photos</a></li>
-							<li><a href="<?php bloginfo('url'); ?>/video/">Videos</a></li>
-                        </ul>
-                       
-					</ul>
-					
-					
-				</div> <!-- end span 3 -->
-				
-				<div class="span3 visible-desktop">
-					
-					<ul>	
-						<li>Take Action</li>
-						<ul>
-							<li><a href="<?php bloginfo('url'); ?>/why-give/">Why Give?</a></li>
-							<li><a href="<?php bloginfo('url'); ?>/volunteer/">Volunteer</a></li>
-							<li><a href="<?php bloginfo('url'); ?>/membership/">Become a Member</a></li>
-							<li><a href="<?php bloginfo('url'); ?>/calendar/">Events Calendar</a> </li>
-						</ul>
+            
+                <div class="span4">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/footerlogo.png" id="footerlogo">
+                    <h1>Ericeira</h1>
+                 </div>
+                 <div class="span4 pull-right" id="socialdiv">
+                                <a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
+                                <a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
+                                <a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb"></a>
+                                <a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb"></a>
+                                <a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
+                </div>
+            </div>
+            
+            <div class="row">
+            
+                        <div>
+                            <ul class="majornav">
+                                <li class="majornav"><a href="<?php bloginfo('url'); ?>/about/">About Us</a>
+                                    <ul>
+                                        <li>About Us</li>
+                                        <li>Our Staff</li>
+                                        <li>Our Partners</li>
+                                        <li> Sustainable surfing</li>
+                                    </ul>
+                                </li>
+                                <li class="majornav">Learn
+                                    <ul>
+                                        <li><a href="<?php bloginfo('url'); ?>/problem/">The Problem</a></li>
+                                        <li><a href="<?php bloginfo('url'); ?>/resources/">Resources</a></li>
+                                        <li><a href="<?php bloginfo('url'); ?>/psa/">PSAs</a></li>
+                                    </ul>
+                                </li>
+                                <li class="majornav">Blog
+                                    <ul>
+                                        <li><a href="<?php bloginfo('url'); ?>/surfrider-blog/">Blog</a></li>
+                                        <li><a href="<?php bloginfo('url'); ?>/photos/">Photos</a></li>
+                                        <li><a href="<?php bloginfo('url'); ?>/video/">Videos</a></li>
+                                    </ul>
+                                </li>
+                                <li class="majornav">Take Action
+                                    <ul>
+                                        <li><a href="<?php bloginfo('url'); ?>/why-give/">Why Give?</a></li>
+                                        <li><a href="<?php bloginfo('url'); ?>/volunteer/">Volunteer</a></li>
+                                        <li><a href="<?php bloginfo('url'); ?>/membership/">Become a Member</a></li>
+                                        <li><a href="<?php bloginfo('url'); ?>/calendar/">Events Calendar</a> </li>
+                                    </ul>
+                                </li>
+                                <li class="majornav"> <a href="<?php bloginfo('url'); ?>/kid-zone/" >Kids' Zone</a>
+                                    <ul>
+                                        <li>About Us</li>
+                                        <li>Take Action</li>
+                                        <li>Suring</li>
+                                        <li>Become a Member</li>
+                                    </ul>
+                                </li>
+                            </ul>
+        
+                            </div> <!-- end span 7-->
 
-						<li> <a href="<?php bloginfo('url'); ?>/contact/">Contact Us</a></li>
-							<li> <a href="<?php bloginfo('url'); ?>/kid-zone/" >Kids' Zone</a></li>
-							
-						
-						</ul>
-
-					</div> <!-- end span 3 -->
 
 
-					<div class="span4 offset2">
-						<div class="row">
-							<h1 class="span4">Ericeira</h1>
-						</div>
-						<div class="row">
-							<h4 class="span4">Surfrider Foundation EUROPE</h4>
-						</div>
+				<div class="span3 pull-right">
                         <div class="row">
-							<p class="span4">Contact Information</p>
+							<p>Contact Information</p>
                         </div>
                          <div class="row">
-                        <div class="span4">
-							<div class="fb-like" data-href="http://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?ref=ts&amp;fref=ts" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true"></div>
-						</div>
-                        </div>
-						<div class="row">
-                        <div class="span4 pull-right">
-							<a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
-							<a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
-							<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb"></a>
-							<a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb"></a>
-                            <a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
-						</div>
-                        </div>
+                            <div class="span3">
+                                <div class="fb-like" data-href="http://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?ref=ts&amp;fref=ts" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true"></div>
+                            </div>
+               </div>
+					
                         
                        
                         
