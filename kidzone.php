3002b51e1bfc385dcc84535e29380152fbf3fac7
Madeline Chapin <mchapin@tweaver.fs.elon.edu>
2013-01-19 12:35:53 -0500

    update kid zone content

--- kidzone.php
+++ kidzone.php
@@ -62,16 +62,18 @@ Template Name: Kid-Zone
 						<div class="row">
 						<div class="span6">
 						<h2>About Us</h2>
-						<p>A group of passionate surfers started Surfrider in 1984 as a way to put and end to pollution in the water they surfed in and loved so much. Tom Curren, three-time world surfing champion, started a new life in France and noticed the terrible state of the beaches. As a response, he created the European headquarters of Surfrider Foundation in 1990 in Biarritz (France).  </p>
+						<p>Passionate surfers started coming together at Surfrider to put an end to pollution in 1984! That’s a pretty long time, right?</p>
 
-<p>Since that time, Surfrider Foundation employees and thousands of people have chipped in over the battle to keep the oceans clean. Their contributions over the years allow us to bathe and surf without risking our own health. Surfrider Foundation has headquarters in all major parts of the world, but in plenty of smaller communities like Ericeira as well.</p>
+<p>Since then, Surfrider has been working hard with its employees and tons of volunteers like you to help fight the battle of keeping the oceans clean. When kids choose to become a protector of the coast, they help keep things like your bath and drinking water clean, and give you clean oceans and beaches to enjoy!</p>
+
+<p>But even though Surfrider has had plenty of help over the years, it still needs help from you. Talk to your Mom and Dad about becoming a member at Surfrider, so you can become a protector of the coast too! </p>
 
 <h2>What do we do?</h2>
 
-<p>Surfrider Ericeira works to ensure a clean ocean and environment in different ways.</p>
+<p>Surfrider Ericeira tries to keep beaches and oceans clean by:</p>
 	<ul>
-		<li>We visit schools and help teachers and children understand the importance of living a sustainable lifestyle. That means using making good choices for the land and water around our home.</li> 
-		<li>We help clean beaches so families and surfers can enjoy the coast in a safe, healthy way for everyone involved</li>
+		<li>•	Hanging out in schools with kids and teachers to tell everyone about why they need to be green and make good choices for the earth.</li> 
+		<li>•	Helping clean beaches so your family safely have fun at the beach.</li>
 	</ul>
 						</div><!-- end of span 6-->
 						<img src="<?php echo get_template_directory_uri(); ?>/img/KZaboutside.png" class="span3" />
@@ -87,8 +89,8 @@ Template Name: Kid-Zone
 						<div class="span6">
 						<h2>What can you do?</h2>
 						<ul>
-						<li>Become a protector of the coast! You can pick up trash when you see it in your neighborhood or on the beaches with your family. Surfrider schedules group cleanups we would love to have your help with as well.</li>
-						<li>Talk to your teachers! Let them know how much you love the beaches and land around Ericeira and ask them to tell others about how they can help keep those places clean. You can print off one of our activity packets for your teachers too!</li> 							<li>Tell your friends! If you see a friend dropping trash on the ground, it is okay to ask them to pick it up and throw it away where it belongs. Tell others why you love Ericeira and how much you want to keep it clean. </li>
+						<li><strong>Become a protector of the coast!</strong> You can pick up trash when you see it in your neighborhood or on the beaches with your family. Surfrider schedules group cleanups we would love to have your help with as well.</li>
+						<li><strong>Talk to your teachers! </strong>Let them know how much you love the beaches and land around Ericeira and ask them to tell others about how they can help keep those places clean. You can print off one of our activity packets for your teachers too!</li> 							<li><strong>Tell your friends!</strong> If you see a friend dropping trash on the ground, it is okay to ask them to pick it up and throw it away where it belongs. Tell others why you love Ericeira and how much you want to keep it clean. </li>
 						</ul>
 		</div><!-- end of span 6-->
 						<img src="<?php echo get_template_directory_uri(); ?>/img/fist.png" class="span3" />
@@ -103,17 +105,16 @@ Template Name: Kid-Zone
 					<div class="accordion-inner">
                     <div class="row">
 						<div class="span">
-						<h2>Sustainable Surfing</h2>
-						<p>Though the action of riding a wave is environmentally friendly, there are ways the lifestyle and industry associated with it can be improved to have less negative impact on the environment. Here are some of Surfrider’s quick tips to surfing sustainably. </p>
+						<h2>Ready To Catch Your First Wave?</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet leo nisi, sit amet blandit sem. Integer viverra erat at tortor consectetur aliquet. Nam lectus quam, aliquam eget tempor eu, laoreet scelerisque dui. Nam urna est, tincidunt ac molestie in, porttitor nec purus. In et justo orci. Donec vel massa diam, sed vehicula ligula. Donec at pellentesque leo. Phasellus bibendum erat non urna convallis nec vestibulum lacus placerat. Mauris sapien risus, elementum eget consequat sit amet, rhoncus a velit. Integer consectetur, dui non tempus fermentum, leo nisi dapibus ligula, at rutrum lorem libero sit amet dolor. Nulla tortor mi, aliquet in pretium id, dictum iaculis orci. Vivamus laoreet interdum luctus. Nullam laoreet iaculis ultrices. Proin at enim ut purus adipiscing scelerisque. Aenean vitae eros in lacus pellentesque vehicula.</p>
 
-						<ul>
-							<li>Purchase your equipment from companies that localize production. Despite how well a product may be made by a certain brand, the petroleum involved in transport can pose a significant threat to the health of the environment. </li>
-							<li>Choose to purchase from brands who place emphasis on using materials that are good for the environment. Manufacturers can reduce the toxicity of surfboard and wetsuit production by using alternative materials instead. By opting to support companies that do with your purchase, you help spur a small amount of change in the industry. </li>
-							<li>Shop locally from retailers who deal with waste and energy use responsibly. It’s great if a surfing business caries great products, but if it’s reckless in how it disposes of the waste that comes with those materials, you’re just contributing to more toxic waste from the surfing industry. </li>
-							<li>Make less surf trips and drive less. If you have the option to surf a full day once a week versus one hour five days a week, you can significantly reduce the amount of fuel you use in travel. </li>
-							<li>Seek out more ways to create less waste on your own. Be as wary of the impact you have on the environment as you can. </li>
-						</ul>
-						<p>By following these tips, you can help make sure the waters you choose to surf in are clean and healthy for you, your friends and the rest of the surfing community. </p>
+<p>Sed eu mauris a velit ultrices vulputate vel vitae nulla. Suspendisse risus lorem, pulvinar quis eleifend nec, posuere a ante. Vestibulum venenatis lectus et tellus luctus quis dignissim magna imperdiet. Proin vel nisi varius nisl faucibus mollis non quis dolor. In cursus viverra augue eget lobortis. Morbi tristique, quam eu elementum semper, est nibh luctus elit, egestas commodo est nulla in est. Aenean lobortis, purus in mattis lobortis, ante purus aliquam ipsum, a placerat metus enim blandit elit. Nulla mattis iaculis sapien. Fusce nec augue tellus. Maecenas volutpat tristique nunc nec tincidunt. Etiam ultricies convallis gravida.</p>
+
+<p>Nunc iaculis, nunc eget malesuada elementum, sapien purus rutrum nisl, eu blandit orci massa sit amet sem. Donec sollicitudin tristique nisl, vitae convallis dolor malesuada non. Phasellus adipiscing tortor at nisi lacinia mollis. Vestibulum id mauris urna. Sed imperdiet magna eget risus adipiscing ac ornare tellus malesuada. Donec aliquam ante neque. Ut varius, eros quis bibendum ornare, mauris augue tempus risus, in posuere nibh metus ut sem.</p>
+
+<p>Proin sed orci vel lectus aliquam ullamcorper sed venenatis massa. In ipsum mi, ultricies et imperdiet ac, feugiat nec tortor. In ac neque tellus. Cras sed mauris lacus. Etiam pharetra ultricies risus non euismod. Donec sed luctus lacus. Duis vulputate feugiat turpis et ultrices. Donec ultricies mi vel dolor tincidunt viverra. Sed lacinia pretium luctus. Suspendisse euismod interdum imperdiet. Vestibulum ultricies lectus vitae nisl dapibus interdum. Aenean lobortis volutpat lectus eu egestas. Aenean in ligula ipsum, eu luctus tellus.</p>
+
+<p>Fusce mattis auctor justo id convallis. Quisque accumsan lorem a erat blandit egestas. In lobortis consequat posuere. Fusce sit amet augue diam. Nam suscipit fringilla mi, et ultrices nulla consectetur a. Duis iaculis nulla sit amet justo tempor posuere. Sed nec est sed ligula molestie dictum in pulvinar sem. Ut eget laoreet neque. Quisque gravida ullamcorper sagittis. Morbi lorem dui, faucibus eget commodo eu, fermentum ac sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
 						</div><!-- end of span 6-->
 						</div> <!-- end of row -->
 					</div>
