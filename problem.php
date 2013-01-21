247400645ea544d9541e9399e91ade2c4d613da6
Madeline Chapin <mchapin@brad-berkner.elon.edu>
2013-01-21 09:15:31 -0500

    updates problem page with graphics

--- problem.php
+++ problem.php
@@ -29,8 +29,18 @@ Template Name: Problem
 <!--  begining of accordian --> 
   <div class="accordion" id="accordion2">
   <!-- begining of accordian group --> 
+<div class="row">
+
   <h2>The Problem</h2>
-  <p>As stewards of the Ericeira coast, Surfrider tries its best to be involved in the community and educate others on the dangers certain environmental issues present to the coastline. </p>
+<div class="span4">
+  <p >As stewards of the Ericeira coast, Surfrider tries its best to be involved in the community and educate others on the dangers certain environmental issues present to the coastline. </p>
+</div>
+<div class="span3">
+<img src="<?php echo get_template_directory_uri(); ?>/img/evolutionPlasticMan.swf" alt="" class="visible-desktop">
+<img src="<?php echo get_template_directory_uri(); ?>/img/evolutionPlasticMan.swf" alt="" class="visible-desktop">
+
+</div>
+</div>
 	  <div class="accordion-group">
 		  <div class="accordion-heading">
 			  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
@@ -40,6 +50,8 @@ Template Name: Problem
 
 				<div id="collapseOne" class="accordion-body collapse">
 					<div class="accordion-inner">
+<div class="row">
+<div class="span5"
 <p>Plastics have been a tremendous help to our way of life in the world. It has been used in the production of homes, cars, offices, playgrounds, schools, toys, food and fluid containers. But despite how they have helped make our lives a little easier, they can pose a significant threat to the things the surfing community values greatly.</p>
 
 <p>Plastic usage is among the biggest threats to Portugal’s coastline. Most plastic pollution on the beaches start out as litter on farmland, beaches and streets. When it rains, that plastic is flushed into storm drains or directly into streams, rivers and oceans. Once that plastic has entered the water, marine life can often mistake it for food.</p> 
@@ -49,7 +61,11 @@ Template Name: Problem
 <p>As part of a way to combat the threat of plastic to the world’s oceans and coastlines, Surfrider Foundation has started a campaign titled “Rise Above Plastics,” to reduce the impact of plastics in the marine environment and raise awareness about the dangers of plastic pollution by advocating for a reduction of single-use plastics and the recycling of all plastics. </p>
 
 <p>Visit the <a href="#">Rise Above Plastics site</a> and click on the “Resources” tab to learn some quick facts about the negative impact plastics has on the environment and learn ways to reduce it. 
-</p>					
+</p>
+</div>
+<img src="<?php echo get_template_directory_uri(); ?>/img/plasticCycle.swf" alt="" class="visible-desktop">
+<img src="<?php echo get_template_directory_uri(); ?>/img/plasticCycleStatic.png" alt="" class="hidden-desktop">
+</div>					
 				</div>
 				</div>
 		</div> 
@@ -64,6 +80,8 @@ Waste and Petrol
 </div>
 <div id="collapseTwo" class="accordion-body collapse">
 <div class="accordion-inner">
+<div class="row">
+<div class="span5">
  <p>The dumping of waste into the ocean is also a primary concern of Surfrider. While the Ericeira community has a proud heritage in the fishing industry, many fishermen unfortunately practice habits that aren’t healthy for the world’s oceans. Many fishermen simply dump waste overboard, rather than stowing it securely until it can be disposed of properly once returning to shore. This waste shows up in the form of plastics, fishing nets and other fibers and toxic chemicals that often have a direct impact on the very fish they catch for the community to later consume.</p> 
 
 <p>If any one group cares about protecting the environment, it’s the surfing community. Despite this, however, the surf industry has potential for negative impact on the environment as well. Petroleum usage, in particular, can pose a threat to the environment because of its prevalence in wetsuits and surfboards.</p>
@@ -71,6 +89,11 @@ Waste and Petrol
 <p>Most surfboards are made up of three key ingredients: a raw blank, some fiberglass cloth and a resin hardener. The blank, otherwise known as the foam core, is basically made up of refined petroleum. Just making those blanks alone can poses a number of health and environmental concerns. During production, a number of greenhouse gases are released into the atmosphere and ultimately weaken the ozone layer.</p>  
 
 <p>Check out SustainableSurf for tips on how to buy the right eco-friendly board.</p>
+</div>
+<img src="<?php echo get_template_directory_uri(); ?>/img/plasticCycle.swf" alt="" class="visible-desktop">
+<img src="<?php echo get_template_directory_uri(); ?>/img/trashStatic.png" alt="" class="hidden-desktop">
+
+</div>
 </div>
 </div>
 </div>
