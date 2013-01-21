316c3fa1f59afb2cdfc9e1be46ec736cabf8684a
Madeline Chapin <mchapin@brad-berkner.elon.edu>
2013-01-21 15:28:16 -0500

    added youtube video to membership page

--- membership.php
+++ membership.php
@@ -23,13 +23,31 @@ Template Name: Membership
 	  <div class="container">
 	  <div class="row">
 		  <div class="span10 offset1">
+		    <h2>Become a Member of Surfrider</h2>
+		     <div class="span3 visible-desktop">
+		  <p>Becoming a member of Surfrider is one of the best ways to contribute to the cause of keeping our beaches clean and spread awareness about the environmental issues the community faces. Once you’ve given us your details and paid a small inscription fee, you will have the option to become a member at various level. Each tier of membership comes with its own set of unique benefits, giving one more reason to join in on the action at Surfrider. </p>
+</div>
 
+		  <div class="span6 visible-desktop"><iframe width="560" height="315"src="http://www.youtube.com/embed/1moPcYWh_l0" frameborder="0" allowfullscreen></iframe></div>
+		  
+		  
+		  <div class="visible-tablet">
+		  <p>Becoming a member of Surfrider is one of the best ways to contribute to the cause of keeping our beaches clean and spread awareness about the environmental issues the community faces. Once you’ve given us your details and paid a small inscription fee, you will have the option to become a member at various level. Each tier of membership comes with its own set of unique benefits, giving one more reason to join in on the action at Surfrider. </p>
+</div>
+
+		  <div><iframe class="visible-tablet" width="560" height="315"src="http://www.youtube.com/embed/1moPcYWh_l0" frameborder="0" allowfullscreen></iframe></div>
+		  
+		  
+		  <div class="visible-phone">
+		  <p>Becoming a member of Surfrider is one of the best ways to contribute to the cause of keeping our beaches clean and spread awareness about the environmental issues the community faces. Once you’ve given us your details and paid a small inscription fee, you will have the option to become a member at various level. Each tier of membership comes with its own set of unique benefits, giving one more reason to join in on the action at Surfrider. </p>
+</div>
+
+		  <div><iframe class="visible-phone" src="http://www.youtube.com/embed/1moPcYWh_l0" frameborder="0" allowfullscreen></iframe></div>
   
-<!--  begining of accordian --> 
-  <div class="accordion" id="accordion2">
+		  
+		<!--  begining of accordian --> 
+  <div class="accordion span10" id="accordion2" >
   <!-- begining of accordian group --> 
-  <h2>Become a Member of Surfrider</h2>
-  <p>Becoming a member of Surfrider is one of the best ways to contribute to the cause of keeping our beaches clean and spread awareness about the environmental issues the community faces. Once you’ve given us your details and paid a small inscription fee, you will have the option to become a member at various level. Each tier of membership comes with its own set of unique benefits, giving one more reason to join in on the action at Surfrider. </p>
 	  <div class="accordion-group">
 		  <div class="accordion-heading">
 			  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
