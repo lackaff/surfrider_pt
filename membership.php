d65b73693a65f12d0dc4ec9f26ea14f804e3394c
Madeline Chapin <mchapin@brad-berkner.elon.edu>
2013-01-21 19:16:24 -0500

    content update

--- membership.php
+++ membership.php
@@ -25,7 +25,7 @@ Template Name: Membership
 		  <div class="span10 offset1">
 		    <h2>Become a Member of Surfrider</h2>
 		     <div class="span3 visible-desktop">
-		  <p>Becoming a member of Surfrider is one of the best ways to contribute to the cause of keeping our beaches clean and spread awareness about the environmental issues the community faces. Once you’ve given us your details and paid a small inscription fee, you will have the option to become a member at various level. Each tier of membership comes with its own set of unique benefits, giving one more reason to join in on the action at Surfrider. </p>
+		  <p>Becoming a member of Surfrider is one of the best ways to contribute to the cause of keeping our beaches clean and spread awareness about the environmental issues the community faces. Once you’ve given provided your details and paid a small inscription fee, you will have the option to become a member at various levels. Each tier of membership comes with its own set of unique benefits, giving one more reason to join in on the action at Surfrider. </p>
 </div>
 
 		  <div class="span6 visible-desktop"><iframe width="560" height="315"src="http://www.youtube.com/embed/1moPcYWh_l0" frameborder="0" allowfullscreen></iframe></div>
@@ -75,7 +75,7 @@ Template Name: Membership
                                 <tr>
                                     <td><img src="<?php echo get_template_directory_uri(); ?>/img/gold.png" /></td>
                                     <td><strong>Gold (50€)</strong>
-                                        <ul><li>T-shirt</li><li>Hoodie</li><li>20% off Surfrider affiliated stores and restaurants</li><li>20% off a surf lesson with a Surfrider partner</li><li>PSFF free one-day pass</li></ul></td>
+                                        <ul><li>T-shirt</li><li>Hoodie</li><li>20% off Surfrider affiliated stores and restaurants</li><li>20% off a surf lesson with a Surfrider partner</li><li><a href="http://www.portuguesesurffilmfestival.com" target="_blank">PSFF</a> free one-day pass</li></ul></td>
                                 </tr>
                                 </table> 
 
@@ -95,7 +95,7 @@ Template Name: Membership
 <div id="collapseTwo" class="accordion-body collapse">
 
 			<div class="accordion-inner">
-		<p>A child under 16 can obtain membership for 15€ for year.</p>
+		<p>A child under 16 can obtain membership for 10€ for year.</p>
 		<p>After the initial inscription fee, there are three different levels of memberships that can be obtained. </p>
             <table class="table span7">
             <tr>
@@ -104,12 +104,12 @@ Template Name: Membership
             </tr>
             <tr>
                 <td><img src="<?php echo get_template_directory_uri(); ?>/img/silver.png" /></td>
-                <td><strong>Silver (25€)</strong><ul><li>T-shirt</li><li>20% off Surfrider affiliated stores and restaurants</li><li>PSFF free one-day pass</li></ul></td>
+                <td><strong>Silver (25€)</strong><ul><li>T-shirt</li><li>20% off Surfrider affiliated stores and restaurants</li><li><a href="http://www.portuguesesurffilmfestival.com" target="_blank">PSFF</a> free one-day pass</li></ul></td>
             </tr>
             <tr>
                 <td><img src="<?php echo get_template_directory_uri(); ?>/img/gold.png" /></td>
                 <td><strong>Gold (50€)</strong>
-                    <ul><li>T-shirt</li><li>Hoodie</li><li>20% off Surfrider affiliated stores and restaurants</li><li>20% off a surf lesson with a Surfrider partner</li><li>PSFF free one-day pass</li></ul></td>
+                    <ul><li>T-shirt</li><li>Hoodie</li><li>20% off Surfrider affiliated stores and restaurants</li><li>20% off a surf lesson with a Surfrider partner</li><li><a href="http://www.portuguesesurffilmfestival.com" target="_blank">PSFF</a> free one-day pass</li></ul></td>
             </tr>
             </table> 
 
