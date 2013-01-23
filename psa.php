19151974f3d3770284508def0106ba73ebaf785a
Madeline Chapin <mchapin@cborden.fs.elon.edu>
2013-01-23 11:39:49 -0500

    changed psa to blog page

--- psa.php
+++ psa.php
@@ -19,62 +19,44 @@ Template Name: PSA
 	    
     </div><!-- /.head pic -->
 
- 
+  <div class="container">
+	  <div class="row">
+		<div class="span10 offset1">
+
+
 	
-<div class="container">
-	<div class="row">
-		  <div class="span10 offset1">
-          
-          <div id="content" class="clearfix row-fluid">
-			
-				<div id="main" class="span12 clearfix" role="main">
+<div id="container">
+	<div id="content" role="main">
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						
-					
-						<section class="post_content">
-							<?php the_content(); ?>
-					
-						</section> <!-- end article section -->
-						
-						
-					
-					</article> <!-- end article -->
-					
-					
-					
-					<?php endwhile; ?>	
-					
-					<?php else : ?>
-					
-					<article id="post-not-found">
-					    <header>
-					    	<h1><?php _e("Not Found", "bonestheme"); ?></h1>
-					    </header>
-					    <section class="post_content">
-					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "bonestheme"); ?></p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
-					
-					<?php endif; ?>
-			
-				</div> <!-- end #main -->
-    
-				<?php //get_sidebar(); // sidebar 1 ?>
-    
-			</div> <!-- end #content -->
-                        
-                        
-		</div> <!-- end of span 10 offset 1 -->
-	</div> <!-- end of row -->
-</div> <!-- end of container -->
+<?php $postslist = get_posts('category_name=psa,&numberposts=10&order=DESC&orderby=post_date');
+foreach ($postslist as $post) :
+setup_postdata($post); ?>
+<div class="post_item row">
+ <h3 class="span10"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+    <div class="posthumbnail span2">
+   
+            <?php 
+            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+              the_post_thumbnail();
+            } else { ?>
+	<img src="<?php bloginfo('template_directory'); ?>/img/logo.jpg" alt="<?php the_title(); ?>" />	<?php }
+            ?>
+    </div>
+<span class='side_date span7'><?php the_time('F j, Y'); ?></span>
+<div class="span7"><?php the_excerpt(); ?></div>
+
+</div>
+<?php endforeach; ?>
 
 
 
+	</div><!-- #content -->
+</div><!-- #container -->
+
+
+  </div><!-- /.span10 offset1 -->
+  </div><!-- /.row -->
+  </div> <!-- /.container -->
 
 
 
