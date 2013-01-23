b3d6fbc8e3a6e164235057a01b736627a24b8189
Madeline Chapin <mchapin@cborden.fs.elon.edu>
2013-01-23 15:02:35 -0500

    wipe out!! 404

--- 404.php
+++ 404.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
-			
+			<?php get_template_part( 'navbar'); ?> 
+ <div class="headerpics">
+        <div class="fullpic">
+	          	<img   src="<?php echo get_template_directory_uri(); ?>/img/blog.jpg" alt="">
+	    </div>
+    </div><!-- /.head pic -->
+
 			<div id="content" class="clearfix row-fluid">
 			
 				<div id="main" class="span12 clearfix" role="main">
@@ -10,7 +16,8 @@
 
 							<div class="hero-unit">
 							
-								<h1><?php _e("Epic 404 - Article Not Found","bonestheme"); ?></h1>
+								<h1><?php _e("Wipe Out!","bonestheme"); ?></h1>
+								<h2><?php _e("Epic 404 - Article Not Found","bonestheme"); ?></h2>
 								<p><?php _e("This is embarassing. We can't find what you were looking for.","bonestheme"); ?></p>
 															
 							</div>
