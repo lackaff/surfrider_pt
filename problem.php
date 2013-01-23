2a35734a6609f503653cd3c6e37f00d21fa91845
Madeline Chapin <mchapin@cborden.fs.elon.edu>
2013-01-23 16:37:20 -0500

    updated problem issues

--- problem.php
+++ problem.php
@@ -1,130 +1,234 @@
 <?php
 
+
+
+
+
+
+
 /*
+
+
+
 Template Name: Problem
+
+
+
 */
+
+
+
 ?>
+
+
+
+
+
+
+
+
+
 
 
 <?php get_header(); ?>
 
+
+
+
+
+
+
 <?php get_template_part( 'navbar'); ?> 
 
 
+
+
+
+
+
+
+
+
+
 <!-- Carousel
+
+
+
     ================================================== -->
+
+
+
     <div class="headerpics">
+
+
+
         <div class="fullpic">
+
+
+
 	          	<img src="<?php echo get_template_directory_uri(); ?>/img/problem.jpg" alt="">
+
+
+
 	    </div>
+
+
+
     </div><!-- /.head pic -->
 
+    
 
-	
-	  <div class="container">
-	  <div class="row">
-		  <div class="span10 offset1">
+    
 
-  
-<!--  begining of accordian --> 
-  <div class="accordion" id="accordion2">
-  <!-- begining of accordian group --> 
+    
+
+<div class="container">
+
 <div class="row">
 
-  <h2>The Problem</h2>
-<div class="span10">
-  <p>As stewards of the Ericeira coast, Surfrider tries its best to be involved in the community and educate others on the dangers certain environmental issues present to the coastline. </p>
-</div>
-<div class="span8 offset1">
-<object width="600" height="400"
-classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-codebase="http://fpdownload.macromedia.com/
-pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" class="visible-desktop">
-<param name="SRC" value="evolutionPlasticMan.swf">
-<embed src="<?php echo get_template_directory_uri(); ?>/img/evolutionPlasticMan.swf" width="600" height="400">
-</embed>
-</object> 
-<img src="<?php echo get_template_directory_uri(); ?>/img/evolutionStatic.png" alt="" class="hidden-desktop">
-</div>
-</div>
-</div>
-	  <div class="accordion-group">
-		  <div class="accordion-heading">
-			  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-				  	Plastics
-			</a>
+ <div class="span10 offset1">
+
+          
+
+  <!--  begining of accordian --> 
+
+<h2>The Problem</h2>
+
+
+
+<p>As stewards of the Ericeira coast, Surfrider tries its best to be involved in the community and educate others on the dangers certain environmental issues present to the coastline. </p>
+
+  <div class="row">
+
+	<div class="span8 offset1">
+
+		<object width="600" height="400" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" class="visible-desktop">
+
+		<param name="SRC" value="evolutionPlasticMan.swf">
+
+		<embed src="<?php echo get_template_directory_uri(); ?>/img/evolutionPlasticMan.swf" width="600" height="400"></embed>
+
+		</object> 
+
+		<img src="<?php echo get_template_directory_uri(); ?>/img/evolutionStatic.png" alt="" class="hidden-desktop" />
+
+	</div>
+
+  
+
+ <div class="row">
+
+ <div class="span10">
+
+  <div class="accordion" id="accordion2">
+
+		
+
+  <!-- begining of accordian group --> 
+
+  	<div class="accordion-group">
+
+		  	<div class="accordion-heading">
+
+			  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">Plastics</a>
+
 			</div>
 
-				<div id="collapseOne" class="accordion-body collapse">
-					<div class="accordion-inner">
-<div class="row">
+            <div id="collapseOne" class="accordion-body collapse">
 
+			<div class="accordion-inner">
 
-<div class="span10">
+            		<p>Plastics have been a tremendous help to our way of life in the world. It has been used in the production of homes, cars, offices, playgrounds, schools, toys, food and fluid containers. But despite how they have helped make our lives a little easier, they can pose a significant threat to the things the surfing community values greatly. </p>
 
-<p>Plastics have been a tremendous help to our way of life in the world. It has been used in the production of homes, cars, offices, playgrounds, schools, toys, food and fluid containers. But despite how they have helped make our lives a little easier, they can pose a significant threat to the things the surfing community values greatly. </p>
+					<p>Plastic usage is among the biggest threats to Portugal’s coastline. Most plastic pollution on the beaches start out as litter on farmland and streets. When it rains, that plastic is flushed into storm drains or directly into streams, rivers and oceans. Once that plastic has entered the water, marine life can often mistake it for food. </p>
 
-<p>Plastic usage is among the biggest threats to Portugal’s coastline. Most plastic pollution on the beaches start out as litter on farmland and streets. When it rains, that plastic is flushed into storm drains or directly into streams, rivers and oceans. Once that plastic has entered the water, marine life can often mistake it for food. </p>
+					<p>If that plastic degraded over time like many forms of waste, having plastic around would not be so bad. Plastic isn’t biodegradable, however. Instead, it breaks down into smaller and smaller pieces over time after exposure to sunlight. That fact makes plastic an environmental nightmare because, with the exception of the small amount of plastic that has been incinerated over the years, most plastic ever made still exists in some shape or form.  </p>
 
-<p>If that plastic degraded over time like many forms of waste, having plastic around would not be so bad. Plastic isn’t biodegradable, however. Instead, it breaks down into smaller and smaller pieces over time after exposure to sunlight. That fact makes plastic an environmental nightmare because, with the exception of the small amount of plastic that has been incinerated over the years, most plastic ever made still exists in some shape or form.  </p>
+					<p>As part of a way to combat the threat of plastic to the world’s oceans and coastlines, Surfrider Foundation has started a campaign titled <a href="http://www.surfrider.org/programs/entry/rise-above-plastics" target="_blank">“Rise Above Plastics”</a> to reduce the impact of plastics in the marine environment and raise awareness about the dangers of plastic pollution by advocating for a reduction of single-use plastics and the recycling of all plastics. </p>
 
-<p>As part of a way to combat the threat of plastic to the world’s oceans and coastlines, Surfrider Foundation has started a campaign titled <a href="http://www.surfrider.org/programs/entry/rise-above-plastics" target="_blank">“Rise Above Plastics”</a> to reduce the impact of plastics in the marine environment and raise awareness about the dangers of plastic pollution by advocating for a reduction of single-use plastics and the recycling of all plastics. </p>
+					<p>Visit the <a href="http://www.surfrider.org/programs/entry/rise-above-plastics" target="_blank">Rise Above Plastics</a> site and click on the “Resources” tab to learn some quick facts about the negative impact plastics has on the environment and learn ways to reduce it. </p>
 
-<p>Visit the <a href="http://www.surfrider.org/programs/entry/rise-above-plastics" target="_blank">Rise Above Plastics</a> site and click on the “Resources” tab to learn some quick facts about the negative impact plastics has on the environment and learn ways to reduce it. </p>
-</div>
+            </div>
 
-</div>					
+            </div><!-- end of accordian group ---> 
+
+     </div><!-- end of accordian group -->
+
+     
+
+     
+
+       	<div class="accordion-group">
+
+		  	<div class="accordion-heading">
+
+			  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">Waste and Petrol</a>
+
+			</div>
+
+            <div id="collapseTwo" class="accordion-body collapse">
+
+			<div class="accordion-inner">
+
+            <div class="row">
+
+					<div class="span8 offset1">
+
+                        <object width="600" height="450" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" class="visible-desktop">
+
+                        <param name="SRC" value="plasticCycle.swf">
+
+                        <embed src="<?php echo get_template_directory_uri(); ?>/img/trash.swf" width="600" height="450"></embed>
+
+                        </object> 
+
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/trashStatic.png" alt="" class="hidden-desktop" />
+
+					</div>  
+
 				</div>
-				</div>
-		</div> 
-		<!-- end of accordian group --> 
-		
-  <!-- begining of accordian group --> 		
-<div class="accordion-group">
-<div class="accordion-heading">
-<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
-Waste and Petrol 
-</a>
-</div>
-<div id="collapseTwo" class="accordion-body collapse">
-<div class="accordion-inner">
-<div class="row">
-<div class="span8 offset1">
-<object width="600" height="450"
-classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-codebase="http://fpdownload.macromedia.com/
-pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" class="visible-desktop">
-<param name="SRC" value="plasticCycle.swf">
-<embed src="<?php echo get_template_directory_uri(); ?>/img/trash.swf" width="600" height="450">
-</embed>
-</object> 
-<img src="<?php echo get_template_directory_uri(); ?>/img/trashStatic.png" alt="" class="hidden-desktop">
-</div>
-<div class="span10">
 
- <p>The dumping of waste into the ocean is also a primary concern of Surfrider. While the Ericeira community has a proud heritage in the fishing industry, many fishermen unfortunately practice habits that aren’t healthy for the world’s oceans. Many fishermen simply dump waste overboard, rather than stowing it securely until it can be disposed of properly once returning to shore. This waste shows up in the form of plastics, fishing nets and other fibers and toxic chemicals that often have a direct impact on the very fish they catch for the community to later consume. </p>
+                <p>The dumping of waste into the ocean is also a primary concern of Surfrider. While the Ericeira community has a proud heritage in the fishing industry, many fishermen unfortunately practice habits that aren’t healthy for the world’s oceans. Many fishermen simply dump waste overboard, rather than stowing it securely until it can be disposed of properly once returning to shore. This waste shows up in the form of plastics, fishing nets and other fibers and toxic chemicals that often have a direct impact on the very fish they catch for the community to later consume. </p>
 
- <p>If any one group cares about protecting the environment, it’s the surfing community. Despite this, however, the surf industry has potential for negative impact on the environment as well. Petroleum usage, in particular, can pose a threat to the environment because of its prevalence in wetsuits and surfboards.
-</p>
- <p>Most surfboards are made up of three key ingredients: a raw blank, some fiberglass cloth and a resin hardener. The blank, otherwise known as the foam core, is basically made up of refined petroleum. Just making those blanks alone poses a number of health and environmental concerns. During production, a number of greenhouse gases are released into the atmosphere and ultimately weaken the ozone layer.  </p>
+			 <p>If any one group cares about protecting the environment, it’s the surfing community. Despite this, however, the surf industry has potential for negative impact on the environment as well. Petroleum usage, in particular, can pose a threat to the environment because of its prevalence in wetsuits and surfboards.</p>
 
-Check out <a href="http://sustainablesurf.org" target="_blank">SustainableSurf</a> for tips on how to buy the right eco-friendly board.</p>
-</div>
+ 			<p>Most surfboards are made up of three key ingredients: a raw blank, some fiberglass cloth and a resin hardener. The blank, otherwise known as the foam core, is basically made up of refined petroleum. Just making those blanks alone poses a number of health and environmental concerns. During production, a number of greenhouse gases are released into the atmosphere and ultimately weaken the ozone layer.  </p>
 
+<p>Check out <a href="http://sustainablesurf.org" target="_blank">SustainableSurf</a> for tips on how to buy the right eco-friendly board.</p>
 
-</div>
-</div>
-</div>
-</div>
+            </div>
 
-  <!-- end of accordian group --> 
+            </div><!-- end of accordian group ---> 
 
-  
-  
-  </div><!-- end of accordian --> 
-  </div><!-- /.span10 offset1 -->
-  </div><!-- /.row -->
-  </div> <!-- /.container -->
+     </div><!-- end of accordian group --> 
+
+     
+
+     
+
+     
+
+            
+
+  </div><!-- end of accordian -->        
+
+   
+
+   
+
+   </div>
+   </div>
+   </div>
+
+   
+
+          
+
+</div> <!-- span10 -->
+
+</div><!-- row -->
+
+</div><!-- container -->
 
 
 
