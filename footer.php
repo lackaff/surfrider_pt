a76394a75d924d74fdf52f9f828cd1ff63fa4e49
Madeline Chapin <mchapin@cborden.fs.elon.edu>
2013-01-23 16:22:45 -0500

    updated footer

--- footer.php
+++ footer.php
@@ -16,93 +16,74 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/img/footerlogo.png" id="footerlogo">
                     <h1>Ericeira</h1>
                  </div>
+                 
                  <div class="span4 pull-right" id="socialdiv">
                                 <a href="https://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?fref=ts" target="_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" class="socialthumb" ></a>
                                 <a href="https://twitter.com/surfridericeira" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.png" class="socialthumb"></a>
                                 <a href="http://instagram.com/surfridericeira/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" class="socialthumb"></a>
                                 <a href="#" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/youtube.png" class="socialthumb"></a>
-                                
+                        <h5>Contact Surfrider </h5>
+							<p>surfriderericeira@gmail.com</p>
+							<p>+351 918667358</p>
+							<p>+351 261867182</p>   
+							 <div class="fb-like" data-href="http://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?ref=ts&amp;fref=ts" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true"></div>      
                 </div>
-            </div>
+ 
             
-            <div class="row-fluid">
-            
-                        <div class="span12">
-                            <ul class="majornav">
-                            <div class="span2">
-                                <li class="majornav">About Us
-                                    <ul>
+            <div class="span8">
+                
+                        <div class="list-with-heading">
+                        <h6>About</h6>
+                        	 <ul class="minornav">
                                         <li><a href="<?php bloginfo('url'); ?>/about/">About Us</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/about/">Our Staff</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/about/">Our Partners</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/about/">Sustainable Surfing</a></li>
                                     </ul>
-                                </li>
-                                </div>
-                                <div class="span2">
-                                <li class="majornav">Learn
-                                    <ul>
+                        </div>
+                         <div class="list-with-heading">
+                        <h6>Learn</h6>
+                        	  <ul class="minornav">
                                         <li><a href="<?php bloginfo('url'); ?>/problem/">The Problem</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/resources/">Resources</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/psa/">PSAs</a></li>
                                     </ul>
-                                </li>
-                                </div>
-                                <div class="span2">
-                                <li class="majornav">Blog
-                                    <ul>
+                        </div>
+                           <div class="list-with-heading">
+                        <h6>Blog</h6>
+                        	  <ul class="minornav">
                                         <li><a href="<?php bloginfo('url'); ?>/surfrider-blog/">Blog</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/photos/">Photos</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/video/">Videos</a></li>
                                     </ul>
-                                </li>
-                                </div>
-                                <div class="span2">
-                                <li class="majornav">Take Action
-                                    <ul>
+                        </div>
+                        <div class="list-with-heading">
+                        <h6>Take Action</h6>
+                        	 <ul class="minornav">
                                         <li><a href="<?php bloginfo('url'); ?>/why-give/">Why Give?</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/volunteer/">Volunteer</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/membership/">Become a Member</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/calendar/">Events Calendar</a> </li>
-                                    </ul>
-                                </li>
-                                </div>
-                                <div class="span2">
-                                <li class="majornav">Kids' Zone
-                                    <ul>
+                                    </ul>                       
+                        </div>
+                        
+						<div class="list-with-heading">
+                        <h6>Kids' Zone</h6>
+                        	 <ul class="minornav">
                                         <li><a href="<?php bloginfo('url'); ?>/kid-zone/" >About Us</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/kid-zone/" >Take Action</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/kid-zone/" >Surfing</a></li>
                                         <li><a href="<?php bloginfo('url'); ?>/kid-zone/" >Become a Member</a></li>
-                                    </ul>
-                                </li>
-                                </div>
-                            </ul>
-        
-                            </div> <!-- end span 7-->
-
-
-
-				<div class="span3 pull-right">
-                        <div class="row">
-							<h5>Contact Surfrider </h5>
-							<p>surfriderericeira@gmail.com</p>
-							<p>+351 918667358</p>
-							<p>+351 261867182</p>
+                                    </ul>                   
                         </div>
-                         <div class="row">
-                            <div class="span3">
-                                <div class="fb-like" data-href="http://www.facebook.com/pages/Surfrider-Foundation-Ericeira/422379127821164?ref=ts&amp;fref=ts" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true"></div>
-                            </div>
-               </div>
-					
-                        
+			
+                 </div>       
                        
                         
 						
 
 						
-					</div><!-- end span 4 -->
+				
 				</div><!-- /.row -->
 			</div><!-- /.container -->
 			
