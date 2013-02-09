82023c4e31e31088d79ce53887edf76681909644
Madeline Chapin <mchapin@tweaver.fs.elon.edu>
2013-02-09 09:32:00 -0500

    editing content

--- whygive.php
+++ whygive.php
@@ -25,17 +25,17 @@ Template Name: WhyGive
 	<div class="row">
 		  <div class="span10 offset1">
           
-				<h2>Why Give</h2>   
+				<h2>Why Give?</h2>   
 				<div class="span10">
 					<div class="span5 pull-right quotes">
 							<img src="<?php echo get_template_directory_uri(); ?>/img/andrea.jpg" class="quotepic">
 							<p>"For me Surfrider Foundation is a way to give something back to the ocean."</p>
-							<small class="pull-right"><cite class="pull-right" title="Source Title" >Andrea Torn</cite></br>Beach Representative</small></br>
+							<small class="pull-right"><cite class="pull-right" title="Source Title" >Andrea Torn</cite></br>Beach Representative</small><br />
 							<a href="http://youtu.be/z2My7_LzqwM" class="pull-right" target="_blank">Watch the interview</a>
 					</div>
 					
 					
-<p>We are ALL responsible for the upkeep and protection of the community’s beautiful coastline. Ericeira is now a World Wave Reserve heritage site. Whether someone has lived in Ericeira for decades or is just visiting it for the first time, it’s probably obvious just how beautiful this place is. The Portuguese economy thrives on tourism from local and international visitors alike, so keeping the country and its waters clean is an absolute necessity.</p>
+<p>We are ALL responsible for the upkeep and protection of the community’s beautiful coastline. Ericeira is now a <a href="http://www.savethewaves.org/world_surfing_reserves" target="_blank">World Wave Reserve</a> heritage site. Whether someone has lived in Ericeira for decades or is just visiting it for the first time, it’s probably obvious just how beautiful this place is. The Portuguese economy thrives on tourism from local and international visitors alike, so keeping the country and its waters clean is an absolute necessity.</p>
 
 <p>But for those who live in the area, a clean environment does more than just boost the tourism industry. It helps protect the resources people rely on to survive and enjoy their lives. The use of plastic  has risen in recent years, and traces are now found in local wildlife and in some percentage of the food caught and consumed locally.</p>
 
@@ -50,9 +50,9 @@ Template Name: WhyGive
 <h5>Choose to give back</h5>
 By choosing to donate your time or money to Surfrider, you create awareness about our cause and help maintain Ericeira as one of the last paradises on earth. Surfrider loves the ocean and wants to protect it.
 <dl>
-<dt>Why GIVE time?</dt> <dd> Time is free and a simple and effective way is to contribute to Surfrider’s cause. Just committing to attend three beach cleans yearly can help greatly. </dd>
+<dt>Why Give time?</dt> <dd> Time is free and a simple and effective way is to contribute to Surfrider’s cause. Just committing to attend three beach cleans yearly can help greatly. </dd>
 
-<dt> Why GIVE money?</dt> <dd>Surfrider Ericeira is run on a voluntary basis and your contribution enables our team to pay for administration, travel expenses to and from beaches and merchandise. All is required for the running of an effective network of people.</dd>
+<dt> Why Give money?</dt> <dd>Surfrider Ericeira is run on a voluntary basis and your contribution enables our team to pay for administration, travel expenses to and from beaches and merchandise. All is required for the running of an effective network of people.</dd>
 </dl>
 
 <h5>Your gift matters</h5>
@@ -61,7 +61,7 @@ By choosing to donate your time or money to Surfrider, you create awareness abou
 
 <p>Surfrider is a global organization of likeminded people who share the same concerns with regards to the preservation of our world’s oceans and wildlife.</p> 
 
-<p><a href="<?php bloginfo('url'); ?>/membership/">Become a member</a>and help our Surfrider chapter come one step closer to its goal of safe beaches for all in the Ericeira community.</p>
+<p><a href="<?php bloginfo('url'); ?>/membership/">Become a member</a> and help our Surfrider chapter come one step closer to its goal of safe beaches for all in the Ericeira community.</p>
 
 <p>If you’re active on a local level, the impact you have generates noise to other chapters around the country, Europe and the world. We are all part of a huge team working toward a more sustainable future.</p>
 
